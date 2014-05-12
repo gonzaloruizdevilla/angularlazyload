3409277fd4abd4cb2126a89db4440d3c4a288aa7
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2014-05-12 13:46:20 +0200

    exponer ficheros jade para mostrarlos en pantalla

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/jade',  express.static(__dirname + '/views'));
 
 app.use('/', routes);
 
